2a7771f3399f55a248dfd8a0fd376f1889175da4
ZhenQiang Zhang <3304828046@qq.com>
2025-02-25 10:01:55 +0800

    deque

--- tinySTL/Deque.h
+++ tinySTL/Deque.h
@@ -57,4 +57,81 @@ namespace tinySTL {
             friend void swap(dq_iter<T>& it1, dq_iter<T>& it2);
         };
     }// namespace Detail
+
+    // class deque
+    template<class T, class Alloc>
+    class deque {
+    private:
+        template<class T>
+        friend class ::tinySTL::Detail::dq_iter:
+    public:
+        typedef T value_type;
+        typedef Detail::dq_iter<T> iterator;
+        typedef Detail::dq_iter<const T> const_iterator;
+        typedef T& reference;
+        typedef size_t size_type;
+        typedef ptrdiff_t difference_type;
+        typedef Alloc allocator_type;
+    private:
+        typedef Alloc dataAlloctor;
+        enum class EBuckSize { BUCK_SIZE = 64 };
+    private:
+        iterator beg_, end_;
+        size_t mapSize_;
+        T **map_;
+    public:
+        deque();
+        explicit deque(size_type n, const value_type& value = value_type());
+        template<class InputIterator>
+        deque(InputIterator first, InputIterator last);
+        deque(const deque& other);
+
+        ~deque();
+
+        deque& operator = (const deque& other);
+        deque& operator = (deque&& other);
+    
+        iterator begin();
+        iterator begin() const;
+        iterator end();
+        iterator end() const;
+    public:
+        size_type size() const { return end() - begin(); }
+        bool empty() const { return size() == 0; }
+
+        reference operator[] (size_type n);
+        const_reference operator[] (size_type n) const;
+        reference front();
+        const_reference front() const;
+        reference back();
+        const_reference back() const;
+
+        void push_front(const value_type& value);
+        void push_back(const value_type& value);
+        void pop_front();
+        void pop_back();
+        void swap(deque& other);
+        void clear();
+    private:
+        T* getnNewBuck();
+        T** getNewMap(const size_t size);
+        size_t getNewMapSize(const size_t size);
+        size_t getBuckSize() const;
+        void init();
+        bool back_full() const;
+        bool front_full() const;
+        void deque_aux(size_t n, const value_type& value, std::true_type);
+        template<class InputIterator>
+        void deque_aux(InputIterator first, InputIterator last, std::false_type);
+        void reallocateAndCopy();
+    public:
+        template<class T, class Alloc>
+        friend bool operator ==(const deque<T, Alloc>& d1, const deque<T, Alloc>& d2);
+        template<class T, class Alloc>
+        friend bool operator !=(const deque<T, Alloc>& d1, const deque<T, Alloc>& d2);
+        template<class T, class Alloc>
+        friend void swap(deque<T, Alloc>& d1, deque<T, Alloc>& d2);
+    };// class deque
 }
+// #include "Deque.cpp"
+#endif // _DEQUE_H_
